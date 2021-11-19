1c86ed32001ec623b5f61331e50e25d893abc3c9
Knoxberg <68099218+Knoxberg@users.noreply.github.com>
2021-11-19 16:57:47 +0700

    Penambahan try, catch

--- perintah/exp_music_player.js
+++ perintah/exp_music_player.js
@@ -10,16 +10,56 @@ module.exports = {
 
     async execute(client, message, args, Discord) {
         const voiceChannel = message.member.voice.channel;
+        
 
         //Kalau orang yang ngetik perintah gak ada di voice channel bakal keluarin pesan error.
-        if(!voiceChannel) return message.channel.send("`Ehh, ups:` Kamu harus bergabung ke Voice Channel dahulu.");
+        if(!voiceChannel) return message.channel.send("`Ohh tidak:` Kamu harus bergabung ke `\Voice Channel\` dahulu.");
 
         //Untuk cek izin di server
         const permissions = voiceChannel.permissionsFor(message.client.user);
-        if(!permissions.has('CONNECT')) return message.channel.send("`Eh, ups:` Kamu harus memiliki izin untuk CONNECT kedalam voice channel.");
-        if(!permissions.has('SPEAK')) return message.channel.send("`Eh, ups:` Kamu harus memiliki izin untuk SPEAK kedalam voice channel.");
-        //Kalau tidak ada keyword yang dikasih bakal keluarin pesan error.
-        if(!args.length) return message.channel.send("`Ehh, ups:` Perintah tidak lengkap.")
+        if(!permissions.has('CONNECT')) return message.channel.send("`Ohh tidak:` Kamu harus memiliki izin untuk `\CONNECT\` kedalam voice channel.");
+        if(!permissions.has('SPEAK')) return message.channel.send("`Ohh tidak:` Kamu harus memiliki izin untuk `\SPEAK\` kedalam voice channel.");
+
+        //Kalau tidak ada keyword yang dikasih bakal keluarin pesan bantuan.
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#FFC0CB')
+	    .setTitle('Waah... Miko bantu kamu yah!')
+	    .setDescription('`?play <judul lagu>` = Miko putar hasil pencarian teratas di Youtube. \n `?play <link Youtube>` = Miko putar link itu buat kamu! \n-----------------------------------------------------------------\n Pastikan kamu sudah ada didalam `Voice Channel` juga ya! :wink:')
+        if(!args.length) return message.channel.send(newEmbed)
+
+        //Kalau diberi link, maka kita cek apakah format url valid
+        const URL_valid = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        //Kalau url valid bot bakal join voice dan putar lagu
+        if(URL_valid(args[0])){
+ 
+            const  connection = await voiceChannel.join();
+            const stream  = ytdl(args[0], {filter: 'audioonly'});
+ 
+            try{
+                //Putar lagu pakai url
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+                message.channel.send('Pemutaran selesai, Miko keluar dari voice channel. :wave:');
+            });
+ 
+            await message.reply(`:minidisc: Sekarang memutar ${args[0]}`);
+            return;
+
+            } catch(err) {
+                console.log(`${err}`)
+                return;
+            }
+        }
+
 
         //Bot join ke voice channel
         const connection = await voiceChannel.join();
@@ -34,18 +74,26 @@ module.exports = {
         const video = await videoFinder(args.join());
         if(video) {
             const stream = ytdl(video.url, {filter: 'audioonly'});
-            connection.play(stream, {seek: 0, volume: 1})
-            .on('finish', () => {
+            
+            try {
+                connection.play(stream, {seek: 0, volume: 1})
+                .on('finish', () => {
                 voiceChannel.leave();
-            })
+                message.channel.send('Pemutaran selesai, Miko keluar dari voice channel. :wave:');
+                })
 
-            //Kirim judul lagu yang diputar
-            await message.reply(`:play_pause:Sekarang memutar ${video.title}`)
+                //Kirim judul lagu yang diputar
+                await message.reply(`:minidisc: Sekarang memutar ${video.title}`)
+
+            } catch(err){
+                console.log(err);
+                return;
+            }
+
         
         } else {
             //Kalau tidak ada hasil pencarian di YT
             message.channel.send("Pencarian YouTube kosong.");
         }
     }
-
 }
