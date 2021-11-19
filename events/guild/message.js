789ba206fc81c8a73f21b5614a9893b52785f576
Knoxberg <68099218+Knoxberg@users.noreply.github.com>
2021-11-19 13:57:30 +0700

    Add files via upload

---
+++ events/guild/message.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require('dotenv').config();
+
+module.exports = (Discord, client, message) => {
+    //Prefix buat perintah bot. (Disimpan secara aman di file .env)
+    const prefix = process.env.PREFIX;
+
+    //Kalau pesan tidak diawali dengan prefiks / pesan dikirim sama bot sendiri bakal di cuekin
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    //Pisahin mana yang prefiks, mana yang perintahnya. terus di rubah ke lowercase
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    const command = client.commands.get(cmd);
+
+    //jalanin perintahnya
+    if(command)command.execute(client, message, args, Discord);
+}
